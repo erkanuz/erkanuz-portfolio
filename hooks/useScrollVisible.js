8919eee4c9216c4eb22492475175d0d0ae1c1fee
erkanuz <erkan.nedim.999@gmail.com>
2024-08-26 19:42:50 +0300

    custom hook for scroll-to-top button visibility

---
+++ hooks/useScrollVisible.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+
+export const useScrollVisible = (customHeight) => {
+  const [scrollVisible, setScrollVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => setScrollVisible(window.scrollY > customHeight);
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [customHeight]);
+
+  return scrollVisible;
+};
